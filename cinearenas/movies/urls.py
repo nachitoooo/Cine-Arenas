aec323347fadcdd8dffd41aca3afc106513b29d1
nacho <ignacioaristegui025@gmail.com>
2024-07-02 08:12:25 -0300

    Factura eléctronica para los tickets y urls comentadas

--- cinearenas/movies/urls.py
+++ cinearenas/movies/urls.py
@@ -1,19 +1,24 @@
+# --------- imports ---------
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
-router.register(r'seats', views.SeatViewSet)
-router.register(r'reservations', views.ReservationViewSet)
-router.register(r'movies', views.MovieViewSet, basename='movie')
+# -------- Creación de un enrutador que maneja automáticamente las URL para las vistas de conjunto --------
+#  -------- definir las URL de una API con el enrutador de DRF --------
 
+router = DefaultRouter()
+router.register(r'seats', views.SeatViewSet)  # Registrar el ViewSet de asientos
+router.register(r'reservations', views.ReservationViewSet)  # Registrar el ViewSet de reservaciones
+router.register(r'movies', views.MovieViewSet, basename='movie')  # registrar el ViewSet de películas con un basename personalizado
+
+# -------- urls --------
 urlpatterns = [
-    path('login/', views.CustomObtainAuthToken.as_view(), name='login'),
-    path('logout/', views.user_logout, name='user_logout'),
-    path('csrf/', views.get_csrf_token, name='csrf'),
-    path('movies/', views.MovieListCreate.as_view(), name='movie-list-create'),
-    path('movies/<int:pk>/', views.MovieDetail.as_view(), name='movie-detail'),
-    path('', include(router.urls)),
-    path('create-payment/', views.create_payment, name='create-payment'),
-    path('payment-success/', views.payment_success, name='payment_success'),
+    path('login/', views.CustomObtainAuthToken.as_view(), name='login'),  # Ruta para iniciar sesión y obtener un token de autenticación
+    path('logout/', views.user_logout, name='user_logout'),  # logout
+    path('csrf/', views.get_csrf_token, name='csrf'),  # obtener token csrf
+    path('movies/', views.MovieListCreate.as_view(), name='movie-list-create'),  # listar y crear peliculas
+    path('movies/<int:pk>/', views.MovieDetail.as_view(), name='movie-detail'),  # CRUD para las películas por ID (seleccionar de a 1, según el entero)
+    path('', include(router.urls)),  # Incluir todas las rutas generadas automáticamente por el enrutador
+    path('create-payment/', views.create_payment, name='create-payment'),  # crear pago con la API de MP
+    path('payment-success/', views.payment_success, name='payment_success'),  # mostrar la factura
 ]
--- cinearenas/movies/views.py
+++ cinearenas/movies/views.py
@@ -1,3 +1,4 @@
+# Importaciones necesarias desde el framework Django REST y otros módulos relevantes
 from rest_framework import viewsets, generics, filters, serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -20,37 +21,44 @@ from .serializers import MovieSerializer, SeatSerializer, ReservationSerializer
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from django.views.decorators.csrf import csrf_exempt
+
+# ------------------------ ViewSet para manejar las operaciones CRUD en el modelo Movie ------------------------
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-    permission_classes = [IsAuthenticated]
+    queryset = Movie.objects.all()  # Consulta para obtener todas las películas
+    serializer_class = MovieSerializer  # Serializador para el modelo Movie
+    permission_classes = [IsAuthenticated]  # Requiere autenticación para acceder a estas vistas
 
+# ViewSet para manejar las operaciones CRUD en el modelo Seat
 class SeatViewSet(viewsets.ModelViewSet):
-    queryset = Seat.objects.all()
-    serializer_class = SeatSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['movie__id']
-    permission_classes = [AllowAny]
+    queryset = Seat.objects.all()  # Consulta para obtener todos los asientos
+    serializer_class = SeatSerializer  # Serializador para el modelo Seat
+    filter_backends = [filters.SearchFilter]  # Permite buscar a través de filtros
+    search_fields = ['movie__id']  # Campo de búsqueda para filtrar por id de película
+    permission_classes = [AllowAny]  # Permite acceso sin autenticación
 
+    # Método para filtrar asientos por id de película si se proporciona
     def get_queryset(self):
         movie_id = self.request.query_params.get('movie_id', None)
         if movie_id is not None:
             return Seat.objects.filter(movie_id=movie_id)
         return super().get_queryset()
 
+# ------------------------ ViewSet para manejar las operaciones CRUD en el modelo Reservation ------------------------
 class ReservationViewSet(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
-    permission_classes = [AllowAny]
+    queryset = Reservation.objects.all()  # Obtener las reservas
+    serializer_class = ReservationSerializer  # Serializador para el modelo Reservation (json)
+    permission_classes = [AllowAny]  #  acceso sin autenticación
 
+    # Método para crear una nueva reservación y marcar los asientos como reservados
     def perform_create(self, serializer):
         seats = self.request.data.get('seats')
         if not seats:
-            raise ValidationError('No seats selected')
+            raise ValidationError('No seats selected')  # Error si no se seleccionan asientos
         
         user = self.request.user if self.request.user.is_authenticated else None
         reservation = serializer.save(user=user)
         
+        # Marcar cada asiento seleccionado como reservado
         for seat_id in seats:
             seat = Seat.objects.get(id=seat_id)
             seat.is_reserved = True
@@ -58,20 +66,24 @@ class ReservationViewSet(viewsets.ModelViewSet):
             reservation.seats.add(seat)
         reservation.save()
 
+#  ------------------------ Crear película ------------------------
 class MovieListCreate(generics.ListCreateAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-    permission_classes = [AllowAny]  # Permitir acceso sin autenticación
+    queryset = Movie.objects.all()  # Consulta para obtener todas las películas
+    serializer_class = MovieSerializer  # Convertir a json la clase de models.py
+    permission_classes = [AllowAny]  # acceder sin autenticación requerida
 
+# ------------------------ Obtener, actualizar, eliminar y editar una película ------------------------
 class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-    permission_classes = [AllowAny] 
+    queryset = Movie.objects.all()  # Consulta para obtener todas las películas
+    serializer_class = MovieSerializer  # Serializador para el modelo Movie
+    permission_classes = [AllowAny]  # Permite acceso sin autenticación
 
+# ----------------------- Petición para devolver el CSRF token en JSON ---------------------
 @ensure_csrf_cookie
 def get_csrf_token(request):
     return JsonResponse({'csrfToken': request.META.get('CSRF_COOKIE', '')})
 
+# --------------- Obtener el token de autenticación y devolver información adicional del usuario -------------------
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
@@ -83,31 +95,32 @@ class CustomObtainAuthToken(ObtainAuthToken):
         except AuthenticationFailed:
             return Response({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
 
+
+# ------------------------ Logout -------------------------------
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 @authentication_classes([TokenAuthentication])
 def user_logout(request):
     try:
-        request.user.auth_token.delete()
+        request.user.auth_token.delete()  # Eliminar el token de autenticación del usuario
         logout(request)
         return Response(status=status.HTTP_204_NO_CONTENT)
     except AttributeError:
         return Response({'error': 'El usuario no tiene un token de autenticación'}, status=status.HTTP_400_BAD_REQUEST)
-    
 
+# Vista pública para listar todas las películas sin necesidad de autenticación
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def public_movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
+    movies = Movie.objects.all()  # Consulta para obtener todas las películas
+    serializer = MovieSerializer(movies, many=True)  # Serializar todas las películas
     return Response(serializer.data)
 
-
-#mercadopago
+# Vista para crear un pago utilizando la API de MercadoPago
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def create_payment(request):
-    sdk = mercadopago.SDK(settings.MERCADOPAGO_ACCESS_TOKEN)
+    sdk = mercadopago.SDK(settings.MERCADOPAGO_ACCESS_TOKEN)  # Inicializar SDK de MercadoPago
     
     email = request.data.get('email')
     seats = request.data.get('seats', [])
@@ -119,6 +132,7 @@ def create_payment(request):
     movie_id = seat_objects.first().movie_id if seat_objects.exists() else None
     movie = Movie.objects.get(id=movie_id) if movie_id else None
 
+    # Crear preferencia de pago con los detalles de la compra
     preference_data = {
         "items": [
             {
@@ -143,14 +157,13 @@ def create_payment(request):
         }
     }
 
-    preference_response = sdk.preference().create(preference_data)
+    preference_response = sdk.preference().create(preference_data)  # Crear preferencia en MercadoPago
     preference = preference_response["response"]
 
-    # Guardar la preferencia en la base de datos (si es necesario)
-
+    # Devolver la preferencia creada en la respuesta
     return Response(preference, status=status.HTTP_201_CREATED)
 
-
+# Vista para manejar el éxito del pago y devolver los datos de la factura
 @csrf_exempt
 @require_GET
 def payment_success(request):
@@ -168,10 +181,12 @@ def payment_success(request):
     seats = preference["metadata"]["seats"]
     total_amount = preference["metadata"]["total_amount"]
 
+    # Crear datos de la factura
     invoice_data = {
         "movie_title": movie_title,
         "seats": seats,
         "total_amount": total_amount,
     }
 
+    # Devolver los datos de la factura en una respuesta JSON
     return JsonResponse(invoice_data)
